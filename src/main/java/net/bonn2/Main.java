2abd841772a27298c3b968a75e0fdc72b6f89291
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2022-06-20 12:06:41 -0400

    Refactor to net.bonn2

--- src/main/java/net/bonn2/Main.java
+++ src/main/java/net/bonn2/Main.java
@@ -1,8 +1,8 @@
-package com.bonn2;
+package net.bonn2;
 
-import com.bonn2.modules.Module;
-import com.bonn2.modules.settings.Settings;
-import com.bonn2.modules.settings.types.Setting;
+import net.bonn2.modules.Module;
+import net.bonn2.modules.settings.Settings;
+import net.bonn2.modules.settings.types.Setting;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
 public class Main extends Module {
